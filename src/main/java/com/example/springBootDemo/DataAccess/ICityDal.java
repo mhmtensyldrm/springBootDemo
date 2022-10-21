7584fd9f56133200aad36af5d92f1b3316d90417
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-21 19:28:09 +0300

    citydal interface modified

--- src/main/java/com/example/springBootDemo/DataAccess/ICityDal.java
+++ src/main/java/com/example/springBootDemo/DataAccess/ICityDal.java
@@ -1,5 +1,11 @@
 package com.example.springBootDemo.DataAccess;
 
+import java.util.List;
+import com.example.springBootDemo.Entities.City;
+
 public interface ICityDal {
-	
+	List<City> getAllCities();
+	void add(City city);
+	void update(City city);
+	void delete(City city);
 }
