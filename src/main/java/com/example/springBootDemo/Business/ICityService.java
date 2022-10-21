6596b240f482347414a6a8c30ec63ae897e05957
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-21 19:59:16 +0300

    cityservice interface modified

--- src/main/java/com/example/springBootDemo/Business/ICityService.java
+++ src/main/java/com/example/springBootDemo/Business/ICityService.java
@@ -1,5 +1,12 @@
 package com.example.springBootDemo.Business;
 
-public interface ICityService {
+import java.util.List;
+import com.example.springBootDemo.Entities.City;
 
+public interface ICityService {
+	List<City> getAllCities();
+	void add(City city);
+	void update(City city);
+	void delete(City city);
+	City getById(int id);
 }
