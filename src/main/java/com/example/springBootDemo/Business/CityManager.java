1d4d2246c853c662c59f8d61ef4d1e6ff29bce72
Mehmet Enes Yildirim <mhmtensyldrm71@gmail.com>
2022-10-21 20:08:03 +0300

    citymanager class modified

--- src/main/java/com/example/springBootDemo/Business/CityManager.java
+++ src/main/java/com/example/springBootDemo/Business/CityManager.java
@@ -1,5 +1,45 @@
 package com.example.springBootDemo.Business;
 
-public class CityManager {
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.example.springBootDemo.DataAccess.ICityDal;
+import com.example.springBootDemo.Entities.City;
+
+@Service
+public class CityManager implements ICityService{
+
+	private ICityDal cityDal;
+	
+	@Autowired
+	public CityManager(ICityDal cityDal) {
+		this.cityDal = cityDal;
+	}
+	
+	@Transactional
+	public List<City> getAllCities() {
+		return this.cityDal.getAllCities();
+	}
+	
+	@Transactional
+	public void add(City city) {
+		this.cityDal.add(city);
+	}
+	
+	@Transactional
+	public void update(City city) {
+		this.cityDal.update(city);
+	}
+	
+	@Transactional
+	public void delete(City city) {
+		this.cityDal.delete(city);
+	}
+	
+	@Transactional
+	public City getById(int id) {
+		return this.cityDal.getById(id);
+	}
 
 }
